bbf7ac5051e007babcc845554535643e5dba0862
Vicente Peinado <vicentepeinadoalcedo@gmail.com>
2026-03-17 17:37:50 +0100

    Migrates creadas

---
+++ gisa/database/migrations/2026_03_17_161412_crear_tabla_ingredientes.php
@@ -0,0 +1,24 @@
+<?php
+
+// database/migrations/xxxx_xx_xx_xxxxxx_crear_tabla_ingredientes.php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('ingredientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre')->unique();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('ingredientes');
+    }
+};
---
+++ gisa/database/migrations/2026_03_17_161413_crear_tabla_producto_ingrediente.php
@@ -0,0 +1,25 @@
+<?php
+
+// database/migrations/xxxx_xx_xx_xxxxxx_crear_tabla_producto_ingrediente.php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('producto_ingrediente', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('producto_id')->constrained('productos')->onDelete('cascade');
+            $table->foreignId('ingrediente_id')->constrained('ingredientes')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('producto_ingrediente');
+    }
+};
